a75171cf79c31de55c1d7f7e21d6855f5fc04382
Nadasalah51 <www.nadasalah51@gmail.com>
2025-04-19 23:35:52 +0200

    edit3

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:nuqta/screens/firstpage.dart';
-import 'package:nuqta/screens/forgetnumber.dart';
-import 'package:nuqta/screens/homepage.dart';
-import 'package:nuqta/screens/loginscreen.dart';
-import 'package:nuqta/screens/profile_screen.dart';
-import 'package:nuqta/screens/request.dart';
-import 'package:nuqta/screens/signupscreen.dart';
-import 'package:nuqta/screens/verifyscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,16 +17,7 @@ class MyApp extends StatelessWidget {
       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       //   useMaterial3: true,
       // ),
-      routes: {
-        'firstpage': (context) => Firstpage(),
-        'homepage': (context) => Homepage(),
-        'loginscreen': (context) => Loginscreen(),
-        'forgetnumber': (context) => Forgetnumber(),
-        'signup': (context) => Signupscreen(),
-        'verifyscreen': (context) => Verifyscreen(),
-        'request': (context) => Request(),
-        'profile': (context) => ProfileScreen(),
-      },
+
       home: Firstpage(),
     );
   }
